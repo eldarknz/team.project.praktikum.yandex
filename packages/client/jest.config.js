e0ccb4d278dd94e70bc6806ec48c7a29683ed213
Galina Teklyuk <lunaticenslaved@gmail.com>
2023-05-30 00:38:56 +0300

    Add jest aliases

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -8,4 +8,18 @@ export default {
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
+  moduleNameMapper: {
+    '^@api/(.*)$': '<rootDir>/src/api/$1',
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@layouts/(.*)$': '<rootDir>/src/layouts/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@routers/(.*)$': '<rootDir>/src/routers/$1',
+    '^@service/(.*)$': '<rootDir>/src/service/$1',
+    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@public/(.*)$': '<rootDir>/src/public/$1',
+  },
 }

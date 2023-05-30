0b882c485181bdc803c1dc2a67935eda6fa85238
chelseajoint <91132438+chelseajoint@users.noreply.github.com>
2023-05-30 22:50:54 +0200

    Corrections Error.scss, Error404.tsx, Error500.tsx, index.scss and variables.scss

--- packages/client/src/pages/Error404/Error404.tsx
+++ packages/client/src/pages/Error404/Error404.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Error } from '../../components/Error/index'
+import { Error } from '@components/Error'
 
 export const Error404Page = () => {
   return <Error error="404" title="Не туда попали" />

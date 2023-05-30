2c99006997af318a544c6fb817253ac0fb74ab27
chelseajoint <91132438+chelseajoint@users.noreply.github.com>
2023-05-30 12:37:08 +0200

    Add error component, error 404 page and error 500 page

---
+++ packages/client/src/pages/Error404/Error404.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Error } from '../../components/Error/index'
+
+export const Error404Page = () => {
+  return <Error error="404" title="Не туда попали" />
+}
---
+++ packages/client/src/pages/Error500/Error500.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Error } from '../../components/Error/index'
+
+export const Error500Page = () => {
+  return <Error error="500" title="Мы уже фиксим" />
+}

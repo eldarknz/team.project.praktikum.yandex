8a5dd0a6aa81e8a13f099762cbe87115bca794d4
shbchk <dmitry@kuchev.com>
2023-05-31 18:00:18 +0400

    Button component and styles

--- packages/client/src/components/Button/Button.tsx
+++ packages/client/src/components/Button/Button.tsx
@@ -1,10 +1,54 @@
-import { ButtonHTMLAttributes } from 'react';
+import {
+  AnchorHTMLAttributes,
+  ButtonHTMLAttributes,
+  ElementType,
+} from 'react';
+import './Button.scss';
 
 type ButtonProps = {
-  className: string;
-  type: 'submit' | 'reset' | 'button';
+  className?: string;
+  type?:
+    | 'submit'
+    | 'reset'
+    | 'button'
+    | undefined;
+  href?: string;
+  Component?: ElementType;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button = ({ children, ...otherProps }: ButtonProps) => {
-  return <button {...otherProps}>{children}</button>;
+export const Button = ({
+  children,
+  href,
+  type = href ? undefined : 'button',
+  className = href
+    ? 'basicButton basicButton--link'
+    : 'basicButton',
+  Component = href ? 'a' : 'button',
+  ...otherProps
+}: ButtonProps) => {
+  if (href) {
+    const hrefProps = {
+      [typeof Component === 'string'
+        ? 'href'
+        : 'to']: href,
+    };
+
+    return (
+      <Component
+        className={className}
+        type={type}
+        {...(otherProps as AnchorHTMLAttributes<HTMLAnchorElement>)}
+        {...hrefProps}>
+        {children}
+      </Component>
+    );
+  }
+
+  return (
+    <Component
+      className={className}
+      {...otherProps}>
+      {children}
+    </Component>
+  );
 };

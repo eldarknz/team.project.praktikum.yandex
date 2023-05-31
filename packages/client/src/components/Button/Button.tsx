dd1c2a35c96817ea2b212ea9eca1135435e71b22
shbchk <dmitry@kuchev.com>
2023-05-31 23:43:10 +0400

    Компоненты Button и Input [TEA-29] (#6)
    * added Button and Input basic components
    
    * added InputControl component, edited Input component
    
    * Button component and styles
    
    * button and input types are now exported

---
+++ packages/client/src/components/Button/Button.tsx
@@ -0,0 +1,54 @@
+import {
+  AnchorHTMLAttributes,
+  ButtonHTMLAttributes,
+  ElementType,
+} from 'react';
+import './Button.scss';
+
+export type ButtonProps = {
+  className?: string;
+  type?:
+    | 'submit'
+    | 'reset'
+    | 'button'
+    | undefined;
+  href?: string;
+  Component?: ElementType;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const Button = ({
+  children,
+  href,
+  type = href ? undefined : 'button',
+  className = href
+    ? 'basicButton basicButton--link'
+    : 'basicButton',
+  Component = href ? 'a' : 'button',
+  ...otherProps
+}: ButtonProps) => {
+  if (href) {
+    const hrefProps = {
+      [typeof Component === 'string'
+        ? 'href'
+        : 'to']: href,
+    };
+
+    return (
+      <Component
+        className={className}
+        type={type}
+        {...(otherProps as AnchorHTMLAttributes<HTMLAnchorElement>)}
+        {...hrefProps}>
+        {children}
+      </Component>
+    );
+  }
+
+  return (
+    <Component
+      className={className}
+      {...otherProps}>
+      {children}
+    </Component>
+  );
+};

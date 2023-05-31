58775b39babfe7d4bf9fb8fbaeb9196b33712527
shbchk <dmitry@kuchev.com>
2023-05-31 12:20:28 +0400

    -

--- packages/client/src/service/Validate.ts
+++ packages/client/src/service/Validate.ts
@@ -1,5 +1,24 @@
 export class Validate {
-  public email = (email: string): boolean => {
-    return false
+  public isString = (content: unknown): boolean => {
+    return typeof content !== 'string'
+  }
+
+  public isLengthLessThan = (
+    content: string,
+    length: number
+  ): boolean | never => {
+    if (!this.isString(content)) {
+      throw new Error('Not a string')
+    }
+
+    return content.length > length
+  }
+
+  public email = (content: string): boolean | never => {
+    if (!this.isString(content)) {
+      throw new Error('Not a string')
+    }
+
+    return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(content)
   }
 }

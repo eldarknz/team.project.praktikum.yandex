da0bc32f1cee9749479dbb8454ca6f840d019bbf
shbchk <dmitry@kuchev.com>
2023-06-03 15:28:33 +0400

    SignUp and field validation

--- packages/client/src/pages/SignUp/SignUp.tsx
+++ packages/client/src/pages/SignUp/SignUp.tsx
@@ -1,4 +1,29 @@
+import { ISignupData } from '@api/AuthAPI';
+import { AuthForm } from '@components/AuthForm';
+import { authController } from '@controllers/AuthController';
+
 export const SignUpPage = () => {
   document.title = 'Регистрация';
-  return <div>SignUpPage</div>;
+  return (
+    <AuthForm
+      authType="signup"
+      handleSubmit={event => {
+        event.preventDefault();
+
+        const formData = new FormData(
+          event.target as HTMLFormElement
+        );
+
+        const data: Record<string, string> = {};
+
+        formData.forEach((value, key) => {
+          data[key] = value.toString();
+        });
+
+        authController.signup(
+          data as ISignupData
+        );
+      }}
+    />
+  );
 };

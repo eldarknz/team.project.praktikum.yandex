55f508daf884155cdc4301824bde5f87920f8b1a
shbchk <dmitry@kuchev.com>
2023-06-04 14:15:25 +0400

    Класс валидации Validate [TEA-17] (#18)
    * added validate class, escapeObjectValues util
    
    * -
    
    * Adding Validate class with following methods: email, password, phone, name, isString
    
    * added login and passwordConfirm methods
    
    * removed passwordConfirm method as requested
    
    * fix

---
+++ packages/client/src/service/Validate.ts
@@ -0,0 +1,42 @@
+class Validate {
+  public isString = (
+    content: unknown
+  ): content is string => {
+    return typeof content === 'string';
+  };
+
+  public email = (content: string): boolean => {
+    return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+      content
+    );
+  };
+
+  public password = (
+    content: string
+  ): boolean => {
+    return /^(?=.*\d)(?=.*[A-Z])[^\s]{7,40}$/.test(
+      content
+    );
+  };
+
+  public phone = (content: string): boolean => {
+    return /^\+?\d{10,15}$/.test(content);
+  };
+
+  public name = (content: string): boolean => {
+    return /^[А-ЯЁA-Z][а-яёa-z]+(-[А-ЯЁA-Z][а-яёa-z]+)?$/.test(
+      content
+    );
+  };
+
+  public login = (content: string): boolean => {
+    return (
+      !/^\d+$/.test(content) &&
+      /^[a-zA-Z0-9][a-zA-Z0-9_-]{2,19}$/.test(
+        content
+      )
+    );
+  };
+}
+
+export const validate = new Validate();

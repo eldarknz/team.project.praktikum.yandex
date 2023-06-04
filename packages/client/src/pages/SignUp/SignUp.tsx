ba11ac571e8d5927752a87072a9c61413a1d70f1
shbchk <dmitry@kuchev.com>
2023-06-04 14:38:48 +0400

    SignUp page useCallback

--- packages/client/src/pages/SignUp/SignUp.tsx
+++ packages/client/src/pages/SignUp/SignUp.tsx
@@ -1,29 +1,34 @@
 import { ISignupData } from '@api/AuthAPI';
 import { AuthForm } from '@components/AuthForm';
 import { authController } from '@controllers/AuthController';
+import { FormEvent, useCallback } from 'react';
 
 export const SignUpPage = () => {
   document.title = 'Регистрация';
+
+  const handleSubmit = useCallback(
+    (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      const formData = new FormData(
+        event.target as HTMLFormElement
+      );
+
+      const data: Record<string, string> = {};
+
+      formData.forEach((value, key) => {
+        data[key] = value.toString();
+      });
+
+      authController.signup(data as ISignupData);
+    },
+    []
+  );
+
   return (
     <AuthForm
       authType="signup"
-      handleSubmit={event => {
-        event.preventDefault();
-
-        const formData = new FormData(
-          event.target as HTMLFormElement
-        );
-
-        const data: Record<string, string> = {};
-
-        formData.forEach((value, key) => {
-          data[key] = value.toString();
-        });
-
-        authController.signup(
-          data as ISignupData
-        );
-      }}
+      handleSubmit={handleSubmit}
     />
   );
 };

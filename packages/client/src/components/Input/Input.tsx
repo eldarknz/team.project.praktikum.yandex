e2159f4d899cfa2504a552516c8908faeba03407
shbchk <dmitry@kuchev.com>
2023-06-04 15:56:10 +0400

    Страница регистрации [TEA-14] (#19)
    * SignUp and field validation
    
    * Some fixes
    
    * Adding Validate class
    
    * moved passwordConfirm method from Validate class to AuthForm component
    
    * fix
    
    * adding use of useCallbacks, moved error texts into Validate class
    
    * SignUp page useCallback
    
    * updated dependencies for useCallback

--- packages/client/src/components/Input/Input.tsx
+++ packages/client/src/components/Input/Input.tsx
@@ -1,7 +1,11 @@
 import { InputControl } from '@components/InputControl/InputControl';
 import {
+  ChangeEvent,
+  FocusEvent,
   InputHTMLAttributes,
+  useCallback,
   useId,
+  useState,
 } from 'react';
 import cn from 'classnames';
 import './Input.scss';
@@ -10,19 +14,65 @@ export type InputProps = {
   inputClassName?: string;
   name: string;
   type?: 'text' | 'password' | 'file' | 'hidden';
-  error?: string | boolean;
+  errorText?: string;
   labelText?: string;
+  validator?: (content: string) => boolean;
 } & InputHTMLAttributes<HTMLInputElement>;
 
 export const Input = ({
   inputClassName = 'baseInput',
-  error,
+  errorText,
   type = 'text',
   labelText,
+  validator,
   ...otherProps
 }: InputProps) => {
+  const [error, setError] = useState<
+    string | null
+  >(null);
+
   const inputId = labelText ? useId() : undefined;
-  const inputClassNames = cn(inputClassName);
+  const inputClassNames = cn(inputClassName, {
+    'baseInput--error': error,
+  });
+
+  const handleBlur = useCallback(
+    (event: FocusEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+
+      if (validator) {
+        const isCorrect = validator(value);
+
+        if (!isCorrect && value.length > 0) {
+          setError(
+            errorText ? errorText : 'Ошибка'
+          );
+        } else {
+          setError(null);
+        }
+      }
+    },
+    [validator, error]
+  );
+
+  const handleChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      if (error && validator) {
+        const { value } = event.target;
+
+        const isCorrectEmail = validator(value);
+
+        if (!isCorrectEmail && value.length > 0) {
+          setError(
+            errorText ? errorText : 'Ошибка'
+          );
+        } else {
+          setError(null);
+        }
+      }
+    },
+    [validator, error]
+  );
 
   return (
     <InputControl
@@ -32,6 +82,8 @@ export const Input = ({
       <input
         id={inputId}
         type={type}
+        onBlur={handleBlur}
+        onChange={handleChange}
         className={inputClassNames}
         {...otherProps}
       />

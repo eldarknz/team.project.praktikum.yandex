eed631cc8f223d6734419130bbb483fc9f5034fa
Eldar <eldarknz@gmail.com>
2023-06-15 14:11:46 +0300

    [kanzafarover/tea-16-forum-page] добавлена главная страница форума

---
+++ packages/client/src/components/Textarea/Component.tsx
@@ -0,0 +1,71 @@
+import cn from 'classnames';
+import {
+  useState,
+  useId,
+  TextareaHTMLAttributes,
+} from 'react';
+import { InputControl } from '@components/InputControl';
+import styles from './styles.module.scss';
+
+export type TTextareaProps = {
+  className?: string;
+  name: string;
+  errorText?: string;
+  labelText?: string;
+  placeholder?: string;
+  validator?: (content: string) => boolean;
+} & TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+export const Textarea = ({
+  className,
+  errorText,
+  labelText,
+  placeholder,
+  name,
+  onChange,
+  validator,
+  ...otherProps
+}: TTextareaProps) => {
+  const [error, setError] = useState<
+    string | null
+  >(null);
+
+  /* eslint-disable */
+  const componentId = labelText
+    ? useId()
+    : undefined;
+  /* eslint-enable */
+
+  const componentClassNames = cn(
+    styles.textarea,
+    {
+      [styles.error]: error,
+    },
+    className
+  );
+
+  const handleBlur = () => {
+    console.log('Blur');
+  };
+
+  const handleChange = () => {
+    console.log('Change');
+  };
+
+  return (
+    <InputControl
+      labelId={componentId}
+      labelText={labelText}
+      error={error}>
+      <textarea
+        id={componentId}
+        name={name}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        className={componentClassNames}
+        placeholder={placeholder}
+        {...otherProps}
+      />
+    </InputControl>
+  );
+};
---
+++ packages/client/src/utils/dateConverter.ts
@@ -0,0 +1,69 @@
+const padTo2Digits = (num: number): string =>
+  num.toString().padStart(2, '0');
+
+export const formatTime = (date: Date) =>
+  [
+    padTo2Digits(date.getUTCHours()),
+    padTo2Digits(date.getUTCMinutes()),
+  ].join(':');
+
+export const formatDate = (date: Date) =>
+  [
+    padTo2Digits(date.getDate()),
+    padTo2Digits(date.getMonth() + 1),
+    date.getFullYear(),
+  ].join('.');
+
+export const formatDayName = (
+  date: Date,
+  locale = 'ru-RU'
+) => {
+  const dayName = date.toLocaleDateString(
+    locale,
+    { weekday: 'short' }
+  );
+  return (
+    dayName.charAt(0).toUpperCase() +
+    dayName.slice(1)
+  );
+};
+
+export const dateConvert = (
+  date: string
+): string => {
+  const pastDate = new Date(date);
+  const today = new Date();
+  if (
+    today.getUTCFullYear() -
+      pastDate.getUTCFullYear() ===
+      0 &&
+    today.getUTCMonth() -
+      pastDate.getUTCMonth() ===
+      0 &&
+    today.getUTCDate() - pastDate.getUTCDate() <=
+      6
+  ) {
+    if (
+      today.getUTCDate() - pastDate.getUTCDate() <
+      1
+    ) {
+      return formatTime(pastDate);
+    }
+    if (
+      today.getUTCDate() -
+        pastDate.getUTCDate() ===
+      1
+    ) {
+      return 'Вчера';
+    }
+    if (
+      today.getUTCDate() -
+        pastDate.getUTCDate() ===
+      2
+    ) {
+      return 'Позавчера';
+    }
+    return formatDayName(pastDate);
+  }
+  return formatDate(pastDate);
+};

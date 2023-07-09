aee0d23db36a6363fe25901844ab186ff4809ed6
Nikita <nikita-prokuratov@mail.ru>
2023-07-09 20:09:47 +0300

    Chore: Fix types

--- packages/client/typed.d.ts
+++ packages/client/typed.d.ts
@@ -5,12 +5,3 @@ declare global {
     __REDUX_STORE__?: RootState;
   }
 }
-
-declare module '*.svg' {
-  import React = require('react');
-  export const ReactComponent: React.FC<
-    React.SVGProps<SVGSVGElement>
-  >;
-  const src: string;
-  export default src;
-}

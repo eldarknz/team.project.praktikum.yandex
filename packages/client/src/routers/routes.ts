119c7852e0ce5677af8cab65194e7710b558c7eb
Nikita <nikita-prokuratov@mail.ru>
2023-07-10 19:57:48 +0300

    test

--- packages/client/src/routers/routes.ts
+++ packages/client/src/routers/routes.ts
@@ -1,30 +1,32 @@
-import { lazy, ComponentType } from 'react'
+import { lazy, ComponentType } from 'react';
 
 const LandingPage = lazy(
-  () => import('@pages/Landing'),
-)
+  () => import('@pages/Landing')
+);
 const AccountPage = lazy(
-  () => import('@pages/Account'),
-)
+  () => import('@pages/Account')
+);
 const ForumPage = lazy(
-  () => import('@pages/Forum'),
-)
+  () => import('@pages/Forum')
+);
 const LeaderboardPage = lazy(
-  () => import('@pages/Leaderboard'),
-)
-const GamePage = lazy(() => import('@pages/Game'))
+  () => import('@pages/Leaderboard')
+);
+const GamePage = lazy(
+  () => import('@pages/Game')
+);
 const SignInPage = lazy(
-  () => import('@pages/SignIn'),
-)
+  () => import('@pages/SignIn')
+);
 const SignUpPage = lazy(
-  () => import('@pages/SignUp'),
-)
+  () => import('@pages/SignUp')
+);
 const Error404Page = lazy(
-  () => import('@pages/Error404'),
-)
+  () => import('@pages/Error404')
+);
 
-const End = lazy(() => import('@pages/End'))
-const Start = lazy(() => import('@pages/Start'))
+const End = lazy(() => import('@pages/End'));
+const Start = lazy(() => import('@pages/Start'));
 
 export enum Layout {
   Default,
@@ -35,11 +37,11 @@ export enum Layout {
 }
 
 export type RouteItem = {
-  path: string
-  isPrivate: boolean
-  component: ComponentType
-  layout: Layout
-}
+  path: string;
+  isPrivate: boolean;
+  component: ComponentType;
+  layout: Layout;
+};
 
 export const ROUTES = {
   Home: {
@@ -102,4 +104,4 @@ export const ROUTES = {
     component: Error404Page,
     layout: Layout.Error,
   },
-}
+};
